88c6b1c14d84174628ced8259a5fe22821950601
ParkYoungWoong <thesecon@gmail.com>
2024-11-18 14:58:26 +0900

    11월 18일 15시까지 수업 내용.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,35 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 import styles from './Header.module.css'
 
+// Hook(훅)
+
+const navigations = [
+  { to: '/', label: 'Home' },
+  { to: '/about', label: 'About' },
+  { to: '/movies', label: 'Movies' }
+]
+
 export default function Header() {
+  const navigate = useNavigate()
+
+  function signIn() {
+    // 로그인 처리..
+    navigate(-2)
+  }
+
   return (
     <header className={styles.header}>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
+      {navigations.map(({ to, label }) => {
+        return (
+          <NavLink
+            key={to}
+            to={to}
+            className={({ isActive }) => (isActive ? styles.active : '')}>
+            {label}
+          </NavLink>
+        )
+      })}
+      <button onClick={signIn}>로그인</button>
     </header>
   )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,11 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import DefaultLayout from './layouts/Default'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import DefaultLayout from './layouts/Default';
+import Home from './pages/Home';
+import About from './pages/About';
+import Movies from './pages/Movies';
 
 const router = createBrowserRouter([
+  // 라우트 객체(페이지 정보)
   {
     // path: '',
     element: <DefaultLayout />,
@@ -15,14 +17,18 @@ const router = createBrowserRouter([
       {
         path: '/about',
         element: <About />
+      },
+      {
+        path: '/movies',
+        element: <Movies />
       }
     ]
   }
-])
+]);
 // http://localhost:5173/
 // http://localhost:5173/about
 
 export default function Router() {
   // Props, Prop, 속성
-  return <RouterProvider router={router} />
+  return <RouterProvider router={router} />;
 }
