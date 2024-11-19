d96af1889ae40ac963fbd1847897f0ebeed04114
ParkYoungWoong <thesecon@gmail.com>
2024-11-19 17:52:52 +0900

    11월 19일 18시까지 수업 내용.

--- src/routes/pages/MovieDetails.tsx
+++ src/routes/pages/MovieDetails.tsx
@@ -1,66 +1,39 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import Modal from '@/components/Modal'
-
-export interface MovieDetails {
-  Title: string
-  Year: string
-  Rated: string
-  Released: string
-  Runtime: string
-  Genre: string
-  Director: string
-  Writer: string
-  Actors: string
-  Plot: string
-  Language: string
-  Country: string
-  Awards: string
-  Poster: string
-  Ratings: Rating[]
-  Metascore: string
-  imdbRating: string
-  imdbVotes: string
-  imdbID: string
-  Type: string
-  DVD: string
-  BoxOffice: string
-  Production: string
-  Website: string
-  Response: string
-}
-export interface Rating {
-  Source: string
-  Value: string
-}
+import { useMovieStore } from '@/stores/movie'
+import Loader from '@/components/Loader'
 
 export default function MovieDetailsPage() {
   const { movieId } = useParams()
-  const [movie, setMovie] = useState<MovieDetails | null>(null)
+  const movie = useMovieStore(state => state.currentMovie)
+  const isLoading = useMovieStore(state => state.isLoading)
+  const fetchMovieDetails = useMovieStore(state => state.fetchMovieDetails)
 
   useEffect(() => {
-    fetchMovieDetails()
+    if (!movieId) return // 타입 가드
+    fetchMovieDetails(movieId)
   }, [movieId])
-
-  async function fetchMovieDetails() {
-    const res = await fetch(`https://omdbapi.com/?apikey=7035c60c&i=${movieId}`)
-    const data = await res.json()
-    setMovie(data)
-  }
 
   return (
     <Modal>
       <h1>Movie Details Page!</h1>
       <h2>{movieId}</h2>
-      {movie && (
+      {isLoading ? (
+        <Loader />
+      ) : (
         <>
-          <h2>{movie.Title}</h2>
-          <img
-            src={movie.Poster}
-            alt={movie.Title}
-          />
-          <p>{movie.Director}</p>
-          <p>{movie.Plot}</p>
+          {movie && (
+            <>
+              <h2>{movie.Title}</h2>
+              <img
+                src={movie.Poster}
+                alt={movie.Title}
+              />
+              <p>{movie.Director}</p>
+              <p>{movie.Plot}</p>
+            </>
+          )}
         </>
       )}
     </Modal>
